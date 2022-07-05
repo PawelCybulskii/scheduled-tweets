7fabfcd6e926cc6a711e5b2488b0de7d87270383
PawelCybulskii <pawcybulski@gmail.com>
2022-07-05 15:50:14 +0200

    added partial form for editting and new tweet which is not working with edit view

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :require_user_logged_in!
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
   def index
     @tweets = Current.user.tweets
@@ -17,10 +18,17 @@ class TweetsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+  end
   
   private
 
   def tweet_params
     params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
+
+  def set_tweet
+    @tweet = Current.user.tweets.find(params[:id])
   end
 end
